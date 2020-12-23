0d5bb0d8437abbeacb0828d9c8de406f1c07c885
Johan <njoubert06@gmail.com>
2020-12-23 09:24:18 +0100

    home page in progress

--- ressources/js/script.js
+++ ressources/js/script.js
@@ -6,6 +6,14 @@ const white1 = document.querySelector('.white1');
 const white2 = document.querySelector('.white2');
 const white3 = document.querySelector('.white3');
 const white4 = document.querySelector('.white4');
+const white5 = document.querySelector('.white5');
+const white6 = document.querySelector('.white6');
+const white7 = document.querySelector('.white7');
+const white8 = document.querySelector('.white8');
+const white9 = document.querySelector('.white9');
+const white10 = document.querySelector('.white10');
+const white11 = document.querySelector('.white11');
+const dropDown = document.querySelector('.dropdown-menu');
 
 window.addEventListener('scroll', () => {
   if(window.scrollY > 450) {
@@ -15,6 +23,14 @@ window.addEventListener('scroll', () => {
     white2.classList.add('scroll');
     white3.classList.add('scroll');
     white4.classList.add('scroll');
+    white5.classList.add('scroll');
+    white6.classList.add('scroll');
+    white7.classList.add('scroll');
+    white8.classList.add('scroll');
+    white9.classList.add('scroll');
+    white10.classList.add('scroll');
+    white11.classList.add('scroll');
+    dropDown.classList.add('scroll');
   }
   else {
     nav.classList.remove('scroll');
@@ -23,8 +39,26 @@ window.addEventListener('scroll', () => {
     white2.classList.remove('scroll');
     white3.classList.remove('scroll');
     white4.classList.remove('scroll');
+    white5.classList.remove('scroll');
+    white6.classList.remove('scroll');
+    white7.classList.remove('scroll');
+    white8.classList.remove('scroll');
+    white9.classList.remove('scroll');
+    white10.classList.remove('scroll');
+    white11.classList.remove('scroll');
+    dropDown.classList.remove('scroll');
+  }
+});
+
+window.addEventListener('drop', () => {
+  if(window.scrollY > 450) {
+    dropDown.classList.add('drop');
+  }
+  else {
+    dropDown.classList.remove('drop');
   }
 })
+
 
  
  $(document).ready(function() { 
